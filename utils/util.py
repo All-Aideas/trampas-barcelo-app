2cee4fdaad1a8b00c4921584a3e211c01efa3ff7
Francisco Cerna Fukuzaki <fcerna.ai@gmail.com>
2023-12-29 00:01:19 -0500

    Update util.py

--- utils/util.py
+++ utils/util.py
@@ -214,11 +214,13 @@ def get_casos_por_centro(mapa, fecha=None):
             centro_lat_lng = row['lat_lng']
             centro_nombre = row['nombre_centro']
             texto_resumen_imagen = ""
+            image_base64 = ""
 
             url_ultima_foto = row['ultima_foto'] # Visualizar foto en HTML
             # url_ultima_foto = row['ultima_foto_base64'] # Visualizar foto en HTML
             print(url_ultima_foto)
-            image_base64 = get_image_base64(url_ultima_foto)
+            if url_ultima_foto:
+                image_base64 = get_image_base64(url_ultima_foto)
             texto_resumen_imagen = f"<div>Última foto tomada el día {ultima_fecha_procesada}<img id='resumen_diario_ultima_foto_yolov5' class='img-fluid' src='data:image/jpeg;base64,{image_base64}' width='100%' /></div>"
             
             texto_resumen_no_imagen = f"<div>No hay fotos del día {ultima_fecha_procesada}.</div>"
